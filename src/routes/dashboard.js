4e433b439962ed44b1c3214359ca6888214a68e8
reachjaykatti <reachjaykatti@gmail.com>
2025-12-27 00:22:51 +0530

    Change export from ES6 to CommonJS module syntax

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -183,6 +183,4 @@ router.get('/player/:userId', ensureAuthenticated, async (req, res) => {
     res.status(500).render('404', { title: 'Error' });
   }
 });
-
-
-export default router;
+module.exports = router;
