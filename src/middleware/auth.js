c1c3787dd554f0c5611949d01f7c64d01d425d4b
reachjaykatti <reachjaykatti@gmail.com>
2025-12-26 14:56:08 +0530

    Refactor ensureAuthenticated for better readability

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,7 +1,9 @@
-
 export function ensureAuthenticated(req, res, next) {
-  if (req.session && req.session.user) return next();
-  return res.redirect('/login');
+  if (req.session && req.session.user) {
+    return next();
+  } else {
+    return res.redirect("/login");
+  }
 }
 
 export function ensureAdmin(req, res, next) {
