70f982f69fc8e95084ccd497db0649593261b003
reachjaykatti <reachjaykatti@gmail.com>
2026-03-23 21:57:25 +0530

    Update global.js

--- public/js/global.js
+++ public/js/global.js
@@ -4,3 +4,20 @@ document.addEventListener("DOMContentLoaded", () => {
     window.fdHomeUrl = el.dataset.fdHomeUrl;
   }
 });
+window.showChangePasswordForm = function () {
+  const loginForm = document.getElementById("loginForm");
+  const changeForm = document.getElementById("changePasswordForm");
+  if (loginForm && changeForm) {
+    loginForm.style.display = "none";
+    changeForm.style.display = "block";
+  }
+};
+
+window.showLoginForm = function () {
+  const loginForm = document.getElementById("loginForm");
+  const changeForm = document.getElementById("changePasswordForm");
+  if (loginForm && changeForm) {
+    changeForm.style.display = "none";
+    loginForm.style.display = "block";
+  }
+};
