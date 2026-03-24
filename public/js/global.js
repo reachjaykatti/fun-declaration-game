3e959100e91254498435e9d756988e7e7c49d147
reachjaykatti <reachjaykatti@gmail.com>
2026-03-24 09:17:26 +0530

    Update global.js

--- public/js/global.js
+++ public/js/global.js
@@ -1,23 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const el = document.querySelector("body");
-  if (el && el.dataset && el.dataset.fdHomeUrl) {
-    window.fdHomeUrl = el.dataset.fdHomeUrl;
+  const loginForm = document.getElementById("loginForm");
+  const changeForm = document.getElementById("changePasswordForm");
+
+  const changeBtn = document.querySelector('[onclick="showChangePasswordForm()"]');
+  const backBtn = document.querySelector('[onclick="showLoginForm()"]');
+
+  if (changeBtn) {
+    changeBtn.onclick = () => {
+      if (loginForm && changeForm) {
+        loginForm.style.display = "none";
+        changeForm.style.display = "block";
+      }
+    };
+  }
+
+  if (backBtn) {
+    backBtn.onclick = () => {
+      if (loginForm && changeForm) {
+        changeForm.style.display = "none";
+        loginForm.style.display = "block";
+      }
+    };
   }
 });
-window.showChangePasswordForm = function () {
-  const loginForm = document.getElementById("loginForm");
-  const changeForm = document.getElementById("changePasswordForm");
-  if (loginForm && changeForm) {
-    loginForm.style.display = "none";
-    changeForm.style.display = "block";
-  }
-};
-
-window.showLoginForm = function () {
-  const loginForm = document.getElementById("loginForm");
-  const changeForm = document.getElementById("changePasswordForm");
-  if (loginForm && changeForm) {
-    changeForm.style.display = "none";
-    loginForm.style.display = "block";
-  }
-};
